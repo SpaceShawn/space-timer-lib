2b21817c4a967165d3a15dc0f70ce06cb610374f
Olivier <olivier@spaceconcordia.ca>
2014-12-17 19:25:34 +0000

    added tests and build scripts

---
+++ tests/gtest/timer-test.cpp
@@ -0,0 +1,18 @@
+// using GoogleTests
+#include "gtest/gtest.h"
+
+class Timer_Test : public ::testing::Test {
+	protected:
+	virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+    }
+
+
+};
+
+// Test netman initialization
+TEST_F(Timer_Test, TimerInit) {
+}
+
